35877c1967e8bd4708e9b83f847179131d6186bd
Muhammad Ahmad Abbas <sp22-bcs-099@cuilahore.edu.pk>
2025-09-27 01:34:16 +0500

    Updates again dependencies issues

--- src/payment/PaymentService.ts
+++ src/payment/PaymentService.ts
@@ -11,7 +11,7 @@ export class PaymentService {
 
   constructor() {
     this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-      apiVersion: "2025-08-27.basil", 
+      apiVersion: "2023-10-16", 
     });
   }
 
